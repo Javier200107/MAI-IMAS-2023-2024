c96cd01b6f4f25d9a81bf7f2fd6059f294fada90
Javier200107 <xaviergonzalezbejar@gmail.com>
2023-11-22 19:52:52 +0100

    Map changed

--- src/main/java/eu/su/mas/dedaleEtu/princ/ConfigurationFile.java
+++ src/main/java/eu/su/mas/dedaleEtu/princ/ConfigurationFile.java
@@ -80,7 +80,7 @@ public final class ConfigurationFile {
 	//public static String INSTANCE_TOPOLOGY="resources/topology/map2018-topology-ica";
 	//public static String INSTANCE_TOPOLOGY="resources/topology/map2021-topologyExam1-tree.dgs";
 	//public static String INSTANCE_TOPOLOGY="resources/interlocking/mapInterlocking2-topology";
-	public static String INSTANCE_TOPOLOGY="resources/topology/URV.dgs";
+	public static String INSTANCE_TOPOLOGY="resources/topology/tarragona_map.dgs";
 	//public static String INSTANCE_TOPOLOGY="resources/topology/map2020-topologyExam1-graph.dgs";
 	//public static String INSTANCE_TOPOLOGY="resources/map2018-topology";
 	//public static String INSTANCE_TOPOLOGY="resources/topology/map2022-topologyExam1-draft.dgs";
@@ -102,7 +102,7 @@ public final class ConfigurationFile {
 	//public static String INSTANCE_CONFIGURATION_ELEMENTS="resources/interlocking/mapInterlocking2-elements";
 	//public static String INSTANCE_CONFIGURATION_ELEMENTS="resources/map2018-elements";
 	//public static String INSTANCE_CONFIGURATION_ELEMENTS="resources/mapInterlocking2-elements";
-	public static String INSTANCE_CONFIGURATION_ELEMENTS="resources/URVelements";
+	public static String INSTANCE_CONFIGURATION_ELEMENTS="resources/TarragonaElements";
 	
 	
 	/************************************
