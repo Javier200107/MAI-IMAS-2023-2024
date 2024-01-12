bead9a3a0d6f2fb7c3e7fc224f128847207b7e16
carlacampas <58336141+carlacampas@users.noreply.github.com>
2024-01-12 17:41:36 +0100

    add comments

--- src/main/java/eu/su/mas/dedaleEtu/mas/behaviours/ExploCoopBehaviour.java
+++ src/main/java/eu/su/mas/dedaleEtu/mas/behaviours/ExploCoopBehaviour.java
@@ -81,7 +81,6 @@ public class ExploCoopBehaviour extends SimpleBehaviour {
     private void tmpRandomMovement(List<Couple<Location,List<Couple<Observation,Integer>>>> lobs){
         Location next_node = moveToNextNodeRandom(lobs);
 
-        // Update buffer only if the agent moved and if the new node is not in the buffer
         if (next_node != null && !this.nodeBuffer.contains(next_node)){
 
             if (this.nodeBuffer.size() == BUFFER_SIZE){
@@ -97,11 +96,10 @@ public class ExploCoopBehaviour extends SimpleBehaviour {
     }
 
     private Location moveToNextNodeRandom(List<Couple<Location,List<Couple<Observation,Integer>>>> lobs){
-        //Random move from the current position
         Random r= new Random();
-        int moveId=1+r.nextInt(lobs.size()-1); //removing the current position from the list of target to accelerate the tests, but not necessary as to stay is an action
+        int moveId=1+r.nextInt(lobs.size()-1);
         Location next_node = lobs.get(moveId).getLeft();
-        Location goal_node = next_node; // select the initial random by default if the following checks fail
+        Location goal_node = next_node;
 
         if (!this.nodeBuffer.contains(next_node)){
             goal_node = next_node;
@@ -121,7 +119,6 @@ public class ExploCoopBehaviour extends SimpleBehaviour {
             goal_node = lobs.get(i).getLeft();
             moved = ((AbstractDedaleAgent)this.myAgent).moveTo(goal_node);
             i = i+1;
-            // If it enters this loop it means that the agent is blocked. Clearing the buffer will help him move more freely.
             this.nodeBuffer.clear();
         }
 
@@ -130,7 +127,6 @@ public class ExploCoopBehaviour extends SimpleBehaviour {
             
         }
 
-        // System.out.println("All nodes in the buffer: " + next_node);
         return goal_node;
     }
 
@@ -142,11 +138,9 @@ public class ExploCoopBehaviour extends SimpleBehaviour {
 			this.myAgent.addBehaviour(new ShareMapBehaviour(this.myAgent, TICK_TIME,this.myMap,list_agentNames));
 			//this.myAgent.addBehaviour(new SharePath(this.myAgent, this.myMap));
 		}
-		//0) Retrieve the current position
 		Location myPosition=((AbstractDedaleAgent)this.myAgent).getCurrentPosition();
 
 		if (myPosition!=null){
-			//List of observable from the agent's current position
 			List<Couple<Location,List<Couple<Observation,Integer>>>> lobs=((AbstractDedaleAgent)this.myAgent).observe();
             
             if (this.random_tmp_steps > 0){
@@ -154,19 +148,14 @@ public class ExploCoopBehaviour extends SimpleBehaviour {
                 return;
             }
 
-			/**
-			 * Just added here to let you see what the agent is doing, otherwise he will be too quick
-			 */
 			try {
-				this.myAgent.doWait(this.TICK_TIME);
+				this.myAgent.doWait(TICK_TIME);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 
-			//1) remove the current node from openlist and add it to closedNodes.
 			this.myMap.addNode(myPosition.getLocationId(), MapAttribute.closed);
 
-			//2) get the surrounding nodes and, if not in closedNodes, add them to open nodes.
 			if (!lobs.get(0).getRight().isEmpty() && !lobs.get(0).getRight().get(0).getLeft().equals(Observation.LOCKSTATUS)){
                 Observation treasure_observed = lobs.get(0).getRight().get(0).getLeft();
                 System.out.println(this.myAgent.getLocalName()+" - I try to open the safe" + lobs.get(0).getLeft() + " : " + treasure_observed + ", " +((AbstractDedaleAgent) this.myAgent).openLock(treasure_observed));
@@ -179,16 +168,13 @@ public class ExploCoopBehaviour extends SimpleBehaviour {
 				Location nodeId = CurrentNode.getLeft();
 
 				boolean isNewNode=this.myMap.addNewNode(nodeId.getLocationId());
-				//the node may exist, but not necessarily the edge
 				if (myPosition!=nodeId) {
 					this.myMap.addEdge(myPosition.getLocationId(), nodeId.getLocationId());
 					if (nextNode==null && isNewNode) nextNode=nodeId.getLocationId();
 				}
 			}
 
-			//3) while openNodes is not empty, continues.
 			if (!this.myMap.hasOpenNode() || explored){
-				// Map completed, start random movement
 				explored=true;
                 nextNode = moveToNextNodeRandom(lobs).getLocationId();
                 if (nextNode != null && !this.nodeBuffer.contains(nextNode)){
@@ -200,17 +186,11 @@ public class ExploCoopBehaviour extends SimpleBehaviour {
                     this.nodeBuffer.add(nextNode);
                 }
 			}else{
-				//4) select next move.
-				//4.1 If there exist one open node directly reachable, go for it,
-				//	 otherwise choose one from the openNode list, compute the shortestPath and go for it
 				if (nextNode==null){
-					//no directly accessible openNode
-					//chose one, compute the path and take the first step.
-					nextNode=this.myMap.getShortestPathToClosestOpenNode(myPosition.getLocationId()).get(0);//getShortestPath(myPosition,this.openNodes.get(0)).get(0);
+					nextNode=this.myMap.getShortestPathToClosestOpenNode(myPosition.getLocationId()).get(0);
                      
 				}
 
-				//5) At each time step, the agent check if he received a graph from a teammate.
 				MessageTemplate msgTemplate=MessageTemplate.and(
 						MessageTemplate.MatchProtocol("SHARE-TOPO"),
 						MessageTemplate.MatchPerformative(ACLMessage.INFORM));
@@ -237,16 +217,12 @@ public class ExploCoopBehaviour extends SimpleBehaviour {
 					}
 				}
 
-                // Resolve blocked path with another explorer by doing a tmp random walk
 				if (!explored && !((AbstractDedaleAgent)this.myAgent).moveTo(new gsLocation(nextNode))){
                     this.blocked_counter += 1;
-                    // System.out.println("Blocked during: " + this.blocked_counter);
                     if (this.blocked_counter > 5){
                         System.out.println(this.myAgent.getLocalName() + " - I was blocked for too long. Doing a random walk.");
                         this.blocked_counter = 0;
                         this.random_tmp_steps = 10;
-
-                        // After 5 cycles blocked, Do random walk during 10 steps                    
                     }
                 } else {
                     this.blocked_counter = 0;
