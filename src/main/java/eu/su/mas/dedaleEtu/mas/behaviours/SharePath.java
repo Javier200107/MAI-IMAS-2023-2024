b1bd7972fb0526e9104b500451708bc102a30302
Andres <16962465+bercowsky@users.noreply.github.com>
2024-01-12 19:34:24 +0100

    add recieve object message and send object message to share path

--- src/main/java/eu/su/mas/dedaleEtu/mas/behaviours/SharePath.java
+++ src/main/java/eu/su/mas/dedaleEtu/mas/behaviours/SharePath.java
@@ -42,20 +42,64 @@ public class SharePath extends TickerBehaviour {
     	return;
     }
 
+    /**********************************************************
+     * 	           SENDING AND RECEIVING MESSAGES
+     *    @param Receivers
+     *    @param message
+     **********************************************************/
     private void SendStringMessage(ArrayList<String> Receivers, String message, String protocol) {
         return;
     }
+
+    /*********************************************************
+     * 	           RECEIVING MESSAGES
+     * @return message
+     *******************************************************/
 
 
     private ArrayList<String> ReceiveStringMessage(String protocol) {
     	return new ArrayList<String>();
     }
 
+    /*********************************************************
+     * 	           RECEIVING OBJECTS
+     * @return message
+     *******************************************************/
     private Object ReceiveObjectMessage(String protocol, Object performative) {
-        return new Object();
+        // Receive path from explorer
+        MessageTemplate msgTemplate = MessageTemplate.and(
+                MessageTemplate.MatchProtocol(protocol),
+                MessageTemplate.MatchPerformative((Integer) performative));
+        ACLMessage msgReceived = this.myAgent.receive(msgTemplate);
+        ArrayList<List> paths = null;
+        if (msgReceived != null) {
+            try {
+                paths = (ArrayList<List>) msgReceived.getContentObject();
+            } catch (UnreadableException e) {
+                e.printStackTrace();
+            }
+            return paths;
+        }
+        return null;
     }
 
+    /*********************************************************
+     * 	           SENDING OBJECTS
+     * @return message
+     *******************************************************/
     private void SendObjectMessage(ArrayList<String> Receivers, Object message, Object performative) {
-        return;
+        ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+        msg.setProtocol("SHARE-PATH");
+        msg.setPerformative((Integer) performative);
+        msg.setSender(this.myAgent.getAID());
+        for (String agentName : Receivers) {
+            msg.addReceiver(new AID(agentName,AID.ISLOCALNAME));
+        }
+        try {
+            msg.setContentObject((Serializable) message);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        ((AbstractDedaleAgent)this.myAgent).sendMessage(msg);
     }
 }
